86806563c0fec6281771596839fb093b12c12507
Arthur <arthurhagelporfiriomendes@gmail.com>
2026-02-10 15:24:32 -0300

    Primeira versão do Portal do Futebol

---
+++ app.py
@@ -0,0 +1,19 @@
+# Importa a classe Flask
+from flask import Flask, render_template
+
+# Cria a aplicação Flask
+# __name__ ajuda o Flask a saber onde está o arquivo principal
+app = Flask(__name__)
+
+# Rota principal do site
+# Quando alguém acessa http://127.0.0.1:5000/
+@app.route("/")
+def index():
+    # Renderiza o arquivo index.html que está na pasta templates
+    return render_template("index.html")
+
+# Garante que o servidor só rode se este arquivo for executado diretamente
+if __name__ == "__main__":
+    # Inicia o servidor Flask
+    # debug=True ajuda a mostrar erros e atualizar automaticamente
+    app.run(debug=True)
