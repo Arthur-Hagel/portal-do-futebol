d3b22073bec5b32f3d428e974486f0a74a2999ea
Arthur Hagel <arthurhagelporfiriomendes@gmail.com>
2026-02-10 15:32:36 -0300

    Remove comment for main route in app.py
    Removed comment about the main route access URL.

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 # Rota principal do site
-# Quando alguém acessa http://127.0.0.1:5000/
 @app.route("/")
 def index():
     # Renderiza o arquivo index.html que está na pasta templates
